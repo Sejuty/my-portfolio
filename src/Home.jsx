43f1d058668baaac26ea97551fdf68eaf0bb80bc
Sejuty <tafannumnishat00@gmail.com>
2024-12-25 13:20:18 +0600

    set vantaJs bg

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,37 @@
+import  { useState, useEffect, useRef } from "react";
+import NET from "vanta/dist/vanta.net.min";
+import * as THREE from "three";
 const Home = () => {
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        NET({
+          el: vantaRef.current,
+          spacing:16,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 600.0,
+          minWidth: 600.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-    <div className="border m-4 border-secondary h-[94vh] overflow-hidden"></div>
+    <div
+      ref={vantaRef}
+      className="border m-4 border-secondary h-[94vh] overflow-hidden"
+    ></div>
   );
 };
 
