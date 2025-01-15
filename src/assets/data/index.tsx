f5affe85cd3cdaa36d20d78865fbc29db473bb14
Sejuty <tafannumnishat00@gmail.com>
2025-01-15 15:43:58 +0600

    populate project data

--- src/assets/data/index.tsx
+++ src/assets/data/index.tsx
@@ -12,18 +12,74 @@ export const portfolioData = {
     "Tailwind CSS",
     "Git",
   ],
- 
 };
 
 export const AboutData =
   "I'm a Frontend Web Developer specializing in building and managing the front-end of websites and web applications that drive product success. Check out my work in the Projects section! I enjoy sharing my knowledge of web development through content to help others in the dev community. Connect with me on LinkedIn or Instagram, where I post helpful tips and resources about web development and programming.I'm open to job opportunities where I can contribute, learn, and grow. If you have a role that matches my skills, feel free to reach out!";
 
-export const projectsData =  [
+export const projectsData = [
   {
     id: 1,
     title: "Pi-Utilities",
-    description: "A comprehensive, web-based Office Management System designed for organizations to efficiently manage their meal services and workplace allocation.",
+    description:
+      "A comprehensive, web-based Office Management System designed for organizations to efficiently manage their meal services and workplace allocation.",
     contribution: "Frontend ( Leafletjs, Nextjs, Tailwind, RTK Query)",
-    worked_on: ["Refactored and optimized the Meal Management module to enhance performance and maintainability.", "Developed a Floor Management module using Leaflet.js, enabling administrators to manage organizational infrastructure effectively.", "Tracking building locations, floorplans, and employee seating arrangements.", "Creating, updating, and deleting floors, seats, and workspaces.", "Assigning and unassigning seats and workspaces with interactive floorplan visuals."]
-  }
-]
+    worked_on: [
+      "Refactored and optimized the Meal Management module to enhance performance and maintainability.",
+      "Developed a Floor Management module using Leaflet.js, enabling administrators to manage organizational infrastructure effectively.",
+      "Tracking building locations, floorplans, and employee seating arrangements.",
+      "Creating, updating, and deleting floors, seats, and workspaces.",
+      "Assigning and unassigning seats and workspaces with interactive floorplan visuals.",
+    ],
+  },
+  
+    {
+      id: 2,
+      title: "PiHR",
+      description:
+        "HR & Payroll Software which simplifies HR processes not only for employees but for Managers as well.",
+      contribution: "Frontend (Nextjs, Tailwind, RTK Query, Zod)",
+      worked_on: [
+        "Integrated along with contributing to over 60 reports related to Employee, Leave, Attendance, Salary, Provident Fund and Tax services utilizing micro-frontend",
+        "Features implementation and bug fixing",
+      ],
+    },
+    {
+      id: 3,
+      title: "VivaKits",
+      description:
+        "An NPM package that brings together all the commonly used components.",
+      contribution: "Frontend (Reactjs, Tailwind)",
+      worked_on: [
+        "Implemented and contributed in various components with multiple props. Some of the components are: Select, Breadcrumbs, Input, OtpInput, Skeleton, Switch etc.",
+        "Contributed in the Documentation",
+      ],
+    },
+    {
+      id: 4,
+      title: "We Select",
+      description: "Campaign and Data Analysis Application.",
+      contribution: "Frontend (Nextjs, Tailwind, RTK Query)",
+      worked_on: ["ATS, Campaign, Company and User features"],
+    },
+    {
+      id: 5,
+      title: "Circle",
+      description: "Location based collection social media platform.",
+      contribution: "Frontend (Nextjs, Tailwind)",
+      worked_on: [
+        "Account Deletion form, Support, Terms and Condition, Privacy Policy, Child Safety and Report Account page",
+      ],
+    },
+    {
+      id: 6,
+      title: "Klikit",
+      description:
+        "A management platform that deals with restaurants, food delivery, creator economy, and virtual brands.",
+      contribution: "Frontend (Reactjs, Formik)",
+      worked_on: [
+        "Admin UI (Enterprise), Self Sign-up, Menu (version 2), Social Platform Management, Validation & Klikit Component Library",
+      ],
+    },
+  
+];
